fea270de4ba52318ee37853f58b94dde98de5549
helenswanson <hhyswanson@gmail.com>
2014-06-08 22:54:57 -0400

    Tests pass

--- lib/bounding_area.rb
+++ lib/bounding_area.rb
@@ -1,6 +1,4 @@
-# I don't believe this code is robust, but it works for the
-# test case. I'm sure we'll chat about this at a later time.
-
+# This code isn't robust, but it works for the test case.
 
 class BoundingArea
   def initialize(areas)
@@ -9,7 +7,6 @@ class BoundingArea
   end
 
   def contains_point?(x, y)
-
     if x == @empty_area[0] and y == @empty_area[1]
       value = false
     elsif x >= @areas[0].left and x <= @areas[0].right and
